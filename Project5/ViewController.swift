363801c28fc8dc05fe8eabcbbbc3978fffbde252
buuraak77 <brkylmz8@hotmail.com>
2022-06-26 15:52:49 +0300

    Initial Commit

---
+++ Project5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by Burak Yılmaz on 26.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
